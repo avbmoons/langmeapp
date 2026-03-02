bafc9ed79490aa2f2ec2a7a71f55a38cee4abb3d
avbmoons <avb.moons@gmail.com>
2026-03-02 21:55:39 +0300

    Revert "init commit" it will be another starting code

--- resources/views/components/footer.blade.php
+++
@@ -1,7 +0,0 @@
-<footer class="blog-footer">
-      <p>Modes&nbsp;{{ $text }}</p>
-      {{-- <p>Modes&nbsp;{{ $text2 }}</p> --}}
-      <p>
-        <a href="#">Back to top</a>
-      </p>
-</footer>
