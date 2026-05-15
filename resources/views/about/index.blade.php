baa93f8d020fd33cfd847da0bad151d801ff5d0c
avbmoons <avb.moons@gmail.com>
2026-05-15 19:50:52 +0300

    change about and terve

--- resources/views/about/index.blade.php
+++ resources/views/about/index.blade.php
@@ -8,20 +8,61 @@
 </div>
 <div class="main-center-front-task">
     <div class="task-info info-about">
-      <div class="info-title-block">
+      <div class="info-title-block about">
         <p class="info-title">About project</p>
       </div>
       <div class="info-content-block-about">
-        <div class="task-description-about">
+        <details name="faq" open>
+          <summary>What we doing</summary>
+          <div class="content-about">
+            <p class="text-about">&#9679;&emsp;We show the basic concepts in several languages at ones</p>
+            <p class="text-about">&#9679;&emsp;We are giving you some modes so that these several languages don't get confused when you speak one of them.</p>
+            <p class="text-about">&#9679;&emsp;We keep a history of your exercises so that you can see the progress.</p>            
+          </div>
+        </details>
+        <details name="faq">
+          <summary>How it working</summary>
+          <div class="content-about">
+            <p class="text-about">&#9679;&emsp;You choose the base language that you want to compare other languages with, such as your native language.</p>
+            <p class="text-about">&#9679;&emsp;You choose the languages with which you want to compare the basic concepts of the base language.</p>
+            <p class="text-about">&#9679;&emsp;You choose the topics of the basic concepts that interest you.</p>
+            <p class="text-about">&#9679;&emsp;Yyou choose a mode and work with the selected vocabulary.</p>
+            <p class="text-about">&#9679;&emsp;You are viewing the results of your tasks</p>
+            <p class="text-about">&#9679;&emsp;You can also save your task history if you register.</p>
+          </div>
+        </details>
+        <details name="faq">
+          <summary>We have now</summary>
+          <div class="content-about">
+            <p class="text-about">&#9679;&emsp;Six languages: Russian, English, Armenian, Greek, Finnish, Latvian</p>
+            <p class="text-about">&#9679;&emsp;Fifty topics: from numbers to verbs</p>
+            <p class="text-about">&#9679;&emsp;Four operating  modes</p>
+            <p class="text-about">&#9679;&emsp;The results of your tasks in the current session, regardless of registration.</p>
+            <p class="text-about">&#9679;&emsp;The history of the results of your tasks, if you register.</p>
+            <p class="text-about">&#9679;&emsp;Sending your feedback and requests to upload a history of your results if you register</p>
+          </div>
+        </details>
+        <details name="faq">
+          <summary>What we planing</summary>
+          <div class="content-about">
+            <p class="text-about">&#9679;&emsp;Expand the language composition</p>
+            <p class="text-about">&#9679;&emsp;Supplement the vocabulary of dictionaries</p>
+            <p class="text-about">&#9679;&emsp;Add operating modes</p>            
+            <p class="text-about">&#9679;&emsp;Provide an independent review of the task history for you</p>
+            <p class="text-about">&#9679;&emsp;Develop a mobile application for a website</p>
+          </div>
+        </details>
+        {{-- <div class="task-description-about" name="what-we-doing">
+          <p class="info-title-about">What we doing</p>
           <p class="info-content">Using langMe, you can solve the issue of several languages mixing if you trying to speak ones. Practice not to confuse words and phrases on different languages.</p>
           <p class="info-content">Now we have the basic words and phrases for you on Russian, English, Armenian, Greek, Finnish and Latvian, and a few tasks so that they don't get mixed up in your practice.</p>
           <p class="info-content then">And then, it's just interesting and useful!</p>
-        </div>
+        </div> --}}
       </div>
     </div>
     <div class="chart-rows-block charts-about">
       <div class="charts-title-block">
-        <p class="charts-title">What we have now:</p>
+        <p class="charts-title">What's there now</p>
       </div>
       <div class="chart-mini-row">
         <div class="chart-mini">
